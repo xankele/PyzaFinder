63ea122434475ec001beac1851f3d3a7d768e550
Julcia <annamikulak99@gmail.com>
2021-03-30 23:13:06 +0200

    init

---
+++ PyzaFinder/Model/Coordinate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PyzaFinder
+{
+    public partial class Coordinate
+    {
+        public int IdCoordinates { get; set; }
+        public int? IdRestaurant { get; set; }
+        public double? CoordinateX { get; set; }
+        public double? CoordinateY { get; set; }
+
+        public virtual DumplingRestaurant IdRestaurantNavigation { get; set; }
+    }
+}
---
+++ PyzaFinder/Model/Link.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PyzaFinder
+{
+    public partial class Link
+    {
+        public int IdLink { get; set; }
+        public int? IdRestaurant { get; set; }
+        public string PageLink { get; set; }
+        public string FacebookLink { get; set; }
+        public string InstagramLink { get; set; }
+
+        public virtual DumplingRestaurant IdRestaurantNavigation { get; set; }
+    }
+}
