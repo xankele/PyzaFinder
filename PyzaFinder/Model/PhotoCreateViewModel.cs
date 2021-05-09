af8b8c49d9726a3ac05afcb0a3fdf01d9d793e3e
Ania <annamikulak99@gmail.com>
2021-05-09 23:38:39 +0200

    wszysciutko

---
+++ PyzaFinder/Model/PhotoCreateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PyzaFinder.Model
+{
+    public class PhotoCreateViewModel
+    {
+        public int? IdRestaurant { get; set; }
+        public IFormFile Photo { get; set; }
+        public DateTime? Datetime { get; set; }
+
+        public virtual DumplingRestaurant IdRestaurantNavigation { get; set; }
+    }
+}
---
+++ PyzaFinder/Pages/AdminPanel.cshtml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace PyzaFinder.Pages
+{
+    public class AdminPanelModel : PageModel
+    {
+        private readonly DumplingContext _db;
+        public AdminPanelModel(DumplingContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<DumplingRestaurant> DumplingRestaurants { get; set; }
+        public async Task OnGet()
+        {
+            DumplingRestaurants = await _db.DumplingRestaurants.ToListAsync();
+        }
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var dumpling = await _db.DumplingRestaurants.FindAsync(id);
+            if (dumpling == null)
+            {
+                return NotFound();
+            }
+            _db.DumplingRestaurants.Remove(dumpling);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("AdminPanel");
+        }
+    }
+}
